a0d80a031d75d60189736aac35dbb0c55ee88bcf
TS2656 <70953238+TS2656@users.noreply.github.com>
2020-10-29 12:54:39 +0100

    Add files via upload

---
+++ dom gesprekje.py
@@ -0,0 +1,17 @@
+Python 3.8.5 (v3.8.5:580fbb018f, Jul 20 2020, 12:11:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ( "wat doe je in een nieuwe klas?" )
+keuze  =  input ()
+if  choice  ==  'elkaar leren kennen' :
+    print ( "pak je eerst je laptop of doe je niks?" )
+elif  choice  ==  'laptop' :
+    print ( "zit ik op voetbal" )
+else :
+    print ( choice, "dat klopt helemaal" )
+
+print ( "welke sport doe ik naast voetbal?" )
+keuze  =  input ()
+if  choice  ==  'honkbal' :
+    print ( "vindt ik deze opleiding leuk ?" )
+elif  choice  ==  'nee het is niet voor Thijn' :
